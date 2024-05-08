b591f13bdac005c9a52f47aff2cdb8b4488c1ef7
soniikot <soniikot@gmail.com>
2024-05-08 12:36:28 -0700

    add form validation

--- script.js
+++ script.js
@@ -1,93 +1,72 @@
 const myLibrary = [];
-const books = document.querySelector('.books')
+const books = document.querySelector(".books");
 const removeButton = document.createElement("button");
 const bookCard = document.createElement("div");
-    
 
-
-class Book{
+class Book {
   constructor(title, author, numberOfPages, status) {
-   this.title = title,
-   this.author = author,
-   this.numberOfPages = numberOfPages,
-   this.status = status;
+    (this.title = title),
+      (this.author = author),
+      (this.numberOfPages = numberOfPages),
+      (this.status = status);
   }
-info(){
-  return (`${this.title} by ${this.author}, ${this.numberOfPages} pages`)
+  info() {
+    return `${this.title} by ${this.author}, ${this.numberOfPages} pages`;
   }
 }
 
-  
-  function addBookToLibrary(book) {
-   myLibrary.unshift(book);
-  }
-const button = document.getElementById('addButton')
+function addBookToLibrary(book) {
+  myLibrary.unshift(book);
+}
+const button = document.getElementById("addButton");
 
-
-
-
-  document.getElementById('addButton').onclick = function(){
+document.getElementById("addButton").onclick = function (event) {
+  const form = document.querySelector("#addNewBook");
+  if (!form.checkValidity()) {
+    form.reportValidity();
+  } else {
     event.preventDefault();
-   let title = document.getElementById("title");
-   let author = document.getElementById("author");
-   let numberOfPages = document.getElementById("number_of_pages");
-   let status = checkValue();
-   let book = new Book (title.value, author.value,numberOfPages.value,status)
-   addBookToLibrary(book);
-   displayTheBook(book);
-   
-   
-
+    let title = document.getElementById("title");
+    let author = document.getElementById("author");
+    let numberOfPages = document.getElementById("number_of_pages");
+    let status = checkValue();
+    let book = new Book(title.value, author.value, numberOfPages.value, status);
+    addBookToLibrary(book);
+    displayTheBook(book);
   }
+};
+function displayTheBook(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("bookCard");
+  bookCard.innerHTML = book.info();
+  books.appendChild(bookCard);
 
-
-  function displayTheBook(book){
-
-    
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("bookCard");
-    bookCard.innerHTML=book.info(); 
-   books.appendChild(bookCard);
-  
-
-   let checkStatus = document.createElement("button");
-   checkStatus.id = 'checkStatus';
-   checkStatus.innerHTML= checkValue();
-   bookCard.appendChild(checkStatus);
-  checkStatus.addEventListener('click', () => {
+  let checkStatus = document.createElement("button");
+  checkStatus.id = "checkStatus";
+  checkStatus.innerHTML = checkValue();
+  bookCard.appendChild(checkStatus);
+  checkStatus.addEventListener("click", () => {
     if (checkStatus.innerHTML === "read") {
-        checkStatus.innerHTML = "not read";
-      } else {checkStatus.innerHTML = "read"};
-  ;
-  });
-    
-
-   const removeButton = document.createElement("button");
-   bookCard.appendChild(removeButton);
-  removeButton.id = 'removeButton';
-  removeButton.innerHTML='Remove'; 
-  removeButton.addEventListener('click', () => {
-    books.removeChild(bookCard);
-    
-  });
-  
-  }
-
-function checkValue(){
-   const checkBox=document.getElementById('status');
-  if (checkBox.checked){
-    return 'Read';
-  }
-    else{
-       return 'Not read';
+      checkStatus.innerHTML = "not read";
+    } else {
+      checkStatus.innerHTML = "read";
     }
+  });
+
+  const removeButton = document.createElement("button");
+  bookCard.appendChild(removeButton);
+  removeButton.id = "removeButton";
+  removeButton.innerHTML = "Remove";
+  removeButton.addEventListener("click", () => {
+    books.removeChild(bookCard);
+  });
+}
+
+function checkValue() {
+  const checkBox = document.getElementById("status");
+  if (checkBox.checked) {
+    return "Read";
+  } else {
+    return "Not read";
   }
-
- 
-
-
- 
- 
- 
-
-
+}
